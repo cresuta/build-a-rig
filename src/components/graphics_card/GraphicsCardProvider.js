46f822c98d79b50b8257073c6a7f6396d597ae03
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-12-20 14:46:23 -0500

    graphics card provider created with two getters

---
+++ src/components/graphics_card/GraphicsCardProvider.js
@@ -0,0 +1,27 @@
+import React, { createContext, useState } from "react"
+
+export const GraphicsCardContext = createContext();
+
+export const GraphicsCardProvider = (props) => {
+
+    const [graphicscards, setGraphicsCards] = useState([])
+
+    const getGraphicsCards = () => {
+        return fetch("http://localhost:8088/graphicsCards")
+        .then(res => res.json())
+        .then(setGraphicsCards)
+    }
+
+    const getGraphicsCardById = (id) => {
+        return fetch(`http://localhost:8088/graphicsCards/${id}`)
+        .then(res => res.json())
+    }
+
+    return (
+        <GraphicsCardContext.Provider value={{
+            graphicscards, getGraphicsCards, getGraphicsCardById
+        }}>
+            {props.children}
+        </GraphicsCardContext.Provider>
+    )
+}
