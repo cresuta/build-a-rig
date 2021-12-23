0825ddf392052c402de2ce3cc725cfd145ee7cb6
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-12-23 12:19:33 -0500

    .build-area-totals css layout

--- src/components/BuildArea.js
+++ src/components/BuildArea.js
@@ -6,7 +6,7 @@ export const BuildArea = () => {
     return (
     <>
 
-      <main className="px-2 dashboard__container">
+      <main className="px-2 py-2 dashboard__container">
         
         <div className="graphics-cards__container">
           <h1>Graphics Cards</h1>
@@ -17,9 +17,21 @@ export const BuildArea = () => {
           <div className="build-area__container">
             <div className="build-area">
               <h1>Build Area</h1>
+              <div>
+                <button>Build!</button>
+              </div>
             </div>
-            <div className="build-area-totals">
-              <h1>Build Area Totals</h1>
+            <div className="build-area-totals ">
+              {/* <h1>Build Area Totals</h1> */}
+              <img className="ethereum-logo" src={require('../imgs/ethereum-logo-2.png')} alt="Build-A-Rig Logo" />
+              <div className="build-area-totals__list">
+                <h2>Hardware Cost</h2>
+                <h2>Hash rate</h2>
+                <h2>Power Consumption</h2>
+              </div>
+              <div className="save-build">
+                <button>Save Build</button>
+              </div>
             </div>
           </div>
 
