e4c0f1702970bcf8c63cc4c0b39add283744bfac
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-12-29 14:30:57 -0500

    implementing react dropdown for motherboards

--- src/components/motherboard/MotherboardCard.js
+++ src/components/motherboard/MotherboardCard.js
@@ -1,14 +1,8 @@
-
-import { Dropdown } from 'react-bootstrap'
+import { Dropdown } from "react-bootstrap"
 
 export const MotherboardCard = ({mobo}) => {
     
     return (
-        // <Button variant="dark" className="card-btn">
-        // {mobo.name}<Badge className="card-brand" bg="success">{mobo.num_of_cards_supported}</Badge>
-        // </Button> 
-        <Dropdown.Item href="#">
-        {mobo.name}
-      </Dropdown.Item>
+        <Dropdown.Item href="#">{mobo.name}</Dropdown.Item>
     )
 }
